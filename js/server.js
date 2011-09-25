8b20efc81573d8d479f8b6934eed48f0cfb25ba3
Darren Hobbs <darren.hobbs@gmail.com>
2011-09-25 20:40:38 +0100

    consolidated static content

--- js/server.js
+++ js/server.js
@@ -1,15 +1,12 @@
 "use strict";
 /* jslint node */
 
-var http = require('http'),
-	htmlLoader = require('./htmlLoader'),
-	responseLoader = require('./responseLoader'),
-	express = require('express');
+var express = require('express');
 
 var app = express.createServer();
 
 app.configure(function () {
-	app.use(express.static(__dirname + '/html'));
+	app.use(express.static(__dirname + '/static'));
 	app.use(express.errorHandler({
 		dumpExceptions: true,
 		showStack: true
